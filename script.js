a33b9df9d49f79a0c2279a2e4a1b106bc78cebe1
Canon <cm090.tech@gmail.com>
2022-09-21 23:14:38 -0400

    Update file structure

--- script.js
+++ script.js
@@ -1,198 +1,6 @@
 let app = {};
 
-app.classes = {
-    "1": {
-        "1": {
-            "name": "CSSE232",
-            "start": "9:00",
-            "end": "10:00",
-            "busy": true
-        },
-        "2": {
-            "name": "BIO101",
-            "start": "10:00",
-            "end": "11:00",
-            "busy": true
-        },
-        "3": {
-            "name": "Free",
-            "start": "11:00",
-            "end": "12:00",
-            "busy": false
-        },
-        "4": {
-            "name": "MA276",
-            "start": "12:00",
-            "end": "13:00",
-            "busy": true
-        },
-        "5": {
-            "name": "Free",
-            "start": "13:00",
-            "end": "15:00",
-            "busy": false
-        },
-        "6": {
-            "name": "CSSE220 TA",
-            "start": "15:00",
-            "end": "17:00",
-            "busy": true
-        },
-        "7": {
-            "name": "CSSE220 Meeting",
-            "start": "17:10",
-            "end": "18:00",
-            "busy": true
-        }
-    },
-    "2": {
-        "1": {
-            "name": "CSSE232",
-            "start": "9:00",
-            "end": "10:00",
-            "busy": true
-        },
-        "2": {
-            "name": "BIO101",
-            "start": "10:00",
-            "end": "11:00",
-            "busy": true
-        },
-        "3": {
-            "name": "Free",
-            "start": "11:00",
-            "end": "12:00",
-            "busy": false
-        },
-        "4": {
-            "name": "MA276",
-            "start": "12:00",
-            "end": "13:00",
-            "busy": true
-        },
-        "5": {
-            "name": "CSSE280",
-            "start": "13:00",
-            "end": "15:00",
-            "busy": true
-        },
-        "6": {
-            "name": "CSSE220 TA",
-            "start": "15:00",
-            "end": "17:00",
-            "busy": true
-        },
-        "7": {
-            "name": "Free",
-            "start": "17:00",
-            "end": "19:30",
-            "busy": false
-        },
-        "8": {
-            "name": "Concert Band",
-            "start": "19:30",
-            "end": "21:00",
-            "busy": true
-        }
-    },
-    "3": {
-        "1": {
-            "name": "CSSE232",
-            "start": "9:00",
-            "end": "10:00",
-            "busy": true
-        },
-        "2": {
-            "name": "Free",
-            "start": "10:00",
-            "end": "11:00",
-            "busy": false
-        },
-        "3": {
-            "name": "BIO101",
-            "start": "11:00",
-            "end": "14:00",
-            "busy": true
-        },
-        "4": {
-            "name": "Free",
-            "start": "14:00",
-            "end": "19:30",
-            "busy": false
-        },
-        "5": {
-            "name": "CSSE220 Office Hours",
-            "start": "19:30",
-            "end": "21:30",
-            "busy": true
-        }
-    },
-    "4": {
-        "1": {
-            "name": "CSSE232",
-            "start": "9:00",
-            "end": "10:00",
-            "busy": true
-        },
-        "2": {
-            "name": "BIO101",
-            "start": "10:00",
-            "end": "11:00",
-            "busy": true
-        },
-        "3": {
-            "name": "Free",
-            "start": "11:00",
-            "end": "12:00",
-            "busy": false
-        },
-        "4": {
-            "name": "MA276",
-            "start": "12:00",
-            "end": "13:00",
-            "busy": true
-        },
-        "5": {
-            "name": "CSSE280",
-            "start": "13:00",
-            "end": "15:00",
-            "busy": true
-        },
-        "6": {
-            "name": "CSSE220 TA",
-            "start": "15:00",
-            "end": "17:00",
-            "busy": true
-        }
-    },
-    "5": {
-        "1": {
-            "name": "CSSE232",
-            "start": "9:00",
-            "end": "10:00",
-            "busy": true
-        },
-        "2": {
-            "name": "Free",
-            "start": "10:00",
-            "end": "11:00",
-            "busy": false
-        },
-        "3": {
-            "name": "MA276",
-            "start": "12:00",
-            "end": "13:00",
-            "busy": true
-        },
-        "4": {
-            "name": "CSSE280",
-            "start": "13:00",
-            "end": "15:00",
-            "busy": true
-        }
-    }
-};
-
+app.classes = classes;
 app.filters = document.querySelectorAll('.days-selector > .chip');
 app.events = document.querySelectorAll('.calendar > .card');
 
@@ -236,7 +44,7 @@ app.updateView = (day) => {
             el.classList.add('busy');
         const startNum = parseInt(app.classes[day][i]["start"].split(':')[0]) + parseInt(app.classes[day][i]["start"].split(':')[1]) / 60;
         const endNum = parseInt(app.classes[day][i]["end"].split(':')[0]) + parseInt(app.classes[day][i]["end"].split(':')[1]) / 60;
-        el.setAttribute('data-start',startNum);
+        el.setAttribute('data-start', startNum);
         el.setAttribute('data-end', endNum);
         el.setAttribute('style', '--size:' + (endNum - startNum));
         const num = document.createElement('div');
@@ -258,6 +66,12 @@ app.updateView = (day) => {
 app.main = () => {
     app.addListeners();
     app.useCurrentDate();
+    document.getElementById('infoIcon').onclick = () => {
+        $('#infoModal').slideDown(50);
+    }
+    document.getElementById('infoModal').onclick = () => {
+        $('#infoModal').slideUp(50);
+    }
 }
 
 app.main();
