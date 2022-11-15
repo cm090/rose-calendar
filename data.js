06a767206c6affc1a7d135728ee190640d498eba
Canon <cm090.tech@gmail.com>
2022-11-15 11:22:34 -0500

    W22 schedule

--- data.js
+++ data.js
@@ -7,101 +7,7 @@
 const classes = {
     "1": {
         "1": {
-            "name": "CSSE232",
-            "start": "9:00",
-            "end": "10:00",
-            "busy": true
-        },
-        "2": {
-            "name": "BIO101",
-            "start": "10:00",
-            "end": "11:00",
-            "busy": true
-        },
-        "3": {
-            "name": "Lunch",
-            "start": "11:00",
-            "end": "12:00",
-            "busy": false
-        },
-        "4": {
-            "name": "MA276",
-            "start": "12:00",
-            "end": "13:00",
-            "busy": true
-        },
-        "5": {
-            "name": "Free",
-            "start": "13:00",
-            "end": "15:00",
-            "busy": false
-        },
-        "6": {
-            "name": "CSSE220 TA",
-            "start": "15:00",
-            "end": "17:00",
-            "busy": true
-        },
-        "7": {
-            "name": "CSSE220 Meeting",
-            "start": "17:10",
-            "end": "18:00",
-            "busy": true
-        }
-    },
-    "2": {
-        "1": {
-            "name": "CSSE232",
-            "start": "9:00",
-            "end": "10:00",
-            "busy": true
-        },
-        "2": {
-            "name": "BIO101",
-            "start": "10:00",
-            "end": "11:00",
-            "busy": true
-        },
-        "3": {
-            "name": "Lunch",
-            "start": "11:00",
-            "end": "12:00",
-            "busy": false
-        },
-        "4": {
-            "name": "MA276",
-            "start": "12:00",
-            "end": "13:00",
-            "busy": true
-        },
-        "5": {
-            "name": "CSSE280",
-            "start": "13:00",
-            "end": "15:00",
-            "busy": true
-        },
-        "6": {
-            "name": "CSSE220 TA",
-            "start": "15:00",
-            "end": "17:00",
-            "busy": true
-        },
-        "7": {
-            "name": "Free",
-            "start": "17:00",
-            "end": "19:30",
-            "busy": false
-        },
-        "8": {
-            "name": "Concert Band",
-            "start": "19:30",
-            "end": "21:00",
-            "busy": true
-        }
-    },
-    "3": {
-        "1": {
-            "name": "CSSE232",
+            "name": "CSSE333",
             "start": "8:00",
             "end": "10:00",
             "busy": true
@@ -113,54 +19,100 @@ const classes = {
             "busy": false
         },
         "3": {
-            "name": "BIO101",
+            "name": "MA374",
             "start": "11:00",
+            "end": "12:00",
+            "busy": true
+        },
+        "4": {
+            "name": "Free",
+            "start": "12:00",
             "end": "14:00",
+            "busy": false
+        },
+        "5": {
+            "name": "ECE332",
+            "start": "14:00",
+            "end": "15:00",
+            "busy": true
+        }
+    },
+    "2": {
+        "1": {
+            "name": "MA374",
+            "start": "11:00",
+            "end": "12:00",
+            "busy": true
+        },
+        "2": {
+            "name": "Free",
+            "start": "12:00",
+            "end": "14:00",
+            "busy": false
+        },
+        "3": {
+            "name": "ECE332",
+            "start": "14:00",
+            "end": "15:00",
+            "busy": true
+        },
+        "4": {
+            "name": "RH330",
+            "start": "15:00",
+            "end": "16:00",
+            "busy": true
+        },
+        "5": {
+            "name": "Free",
+            "start": "16:00",
+            "end": "19:30",
+            "busy": false
+        },
+        "6": {
+            "name": "Concert Band",
+            "start": "19:30",
+            "end": "21:00",
+            "busy": true
+        }
+    },
+    "3": {
+        "1": {
+            "name": "CSSE333",
+            "start": "8:00",
+            "end": "10:00",
             "busy": true
         }
     },
     "4": {
         "1": {
-            "name": "CSSE232",
-            "start": "9:00",
-            "end": "10:00",
+            "name": "MA374",
+            "start": "11:00",
+            "end": "12:00",
             "busy": true
         },
         "2": {
-            "name": "BIO101",
-            "start": "10:00",
-            "end": "11:00",
-            "busy": true
-        },
-        "3": {
-            "name": "Lunch",
-            "start": "11:00",
-            "end": "12:00",
+            "name": "Free",
+            "start": "12:00",
+            "end": "14:00",
             "busy": false
         },
-        "4": {
-            "name": "MA276",
-            "start": "12:00",
-            "end": "13:00",
-            "busy": true
-        },
-        "5": {
-            "name": "CSSE280",
-            "start": "13:00",
+        "3": {
+            "name": "ECE332",
+            "start": "14:00",
             "end": "15:00",
             "busy": true
         },
-        "6": {
-            "name": "CSSE220 TA",
+        "4": {
+            "name": "RH330",
             "start": "15:00",
-            "end": "17:00",
+            "end": "16:00",
             "busy": true
         }
     },
     "5": {
         "1": {
-            "name": "CSSE232",
-            "start": "9:00",
+            "name": "CSSE333",
+            "start": "8:00",
             "end": "10:00",
             "busy": true
         },
@@ -171,21 +123,27 @@ const classes = {
             "busy": false
         },
         "3": {
-            "name": "Lunch",
+            "name": "MA374",
             "start": "11:00",
             "end": "12:00",
-            "busy": false
-        },
-        "4": {
-            "name": "MA276",
-            "start": "12:00",
-            "end": "13:00",
             "busy": true
         },
+        "4": {
+            "name": "Free",
+            "start": "12:00",
+            "end": "14:00",
+            "busy": false
+        },
         "5": {
-            "name": "CSSE280",
-            "start": "13:00",
+            "name": "ECE332",
+            "start": "14:00",
             "end": "15:00",
+            "busy": true
+        },
+        "6": {
+            "name": "RH330",
+            "start": "15:00",
+            "end": "16:00",
             "busy": true
         }
     }
