1a4ae73e8ffbc16a23a386fc7692604250ee5beb
Canon Maranda <22649817+cm090@users.noreply.github.com>
2023-08-20 20:21:59 -0400

    Update code formatting

--- data.js
+++ data.js
@@ -1,168 +1,168 @@
 /**
- * 
+ *
  * A JavaScript object that contains the data for the calendar. Sorted by day from Sunday (0) to Saturday (6).
  * View structure below for required data types.
- * 
-*/
+ *
+ */
 const classes = {
-    "1": {
-        "1": {
-            "name": "CSSE220 TA",
-            "start": "8:00",
-            "end": "10:00",
-            "busy": true
-        },
-        "2": {
-            "name": "ECONS152",
-            "start": "10:00",
-            "end": "11:00",
-            "busy": true
-        },
-        "3": {
-            "name": "CSSE332",
-            "start": "11:00",
-            "end": "12:00",
-            "busy": true
-        },
-        "4": {
-            "name": "Free",
-            "start": "12:00",
-            "end": "13:00",
-            "busy": false
-        },
-        "5": {
-            "name": "CSSE371",
-            "start": "13:00",
-            "end": "15:00",
-            "busy": true
-        },
-        "6": {
-            "name": "MA381",
-            "start": "15:00",
-            "end": "16:00",
-            "busy": true
-        },
-        "7": {
-            "name": "CSSE220 TA Meeting",
-            "start": "16:00",
-            "end": "17:00",
-            "busy": true
-        }
+  1: {
+    1: {
+      name: "CSSE220 TA",
+      start: "8:00",
+      end: "10:00",
+      busy: true,
     },
-    "2": {
-        "1": {
-            "name": "CSSE220 TA",
-            "start": "8:00",
-            "end": "10:00",
-            "busy": true
-        },
-        "2": {
-            "name": "ECONS152",
-            "start": "10:00",
-            "end": "11:00",
-            "busy": true
-        },
-        "3": {
-            "name": "CSSE332",
-            "start": "11:00",
-            "end": "12:00",
-            "busy": true
-        },
-        "4": {
-            "name": "Free",
-            "start": "12:00",
-            "end": "15:00",
-            "busy": false
-        },
-        "5": {
-            "name": "MA381",
-            "start": "15:00",
-            "end": "16:00",
-            "busy": true
-        },
-        "6": {
-            "name": "Free",
-            "start": "16:00",
-            "end": "19:30",
-            "busy": false
-        },
-        "7": {
-            "name": "Concert Band",
-            "start": "19:30",
-            "end": "21:00",
-            "busy": true
-        }
+    2: {
+      name: "ECONS152",
+      start: "10:00",
+      end: "11:00",
+      busy: true,
     },
-    "3": {
-        "1": {
-            "name": "CSSE371",
-            "start": "13:00",
-            "end": "15:00",
-            "busy": true
-        }
+    3: {
+      name: "CSSE332",
+      start: "11:00",
+      end: "12:00",
+      busy: true,
     },
-    "4": {
-        "1": {
-            "name": "CSSE220 TA",
-            "start": "8:00",
-            "end": "10:00",
-            "busy": true
-        },
-        "2": {
-            "name": "ECONS152",
-            "start": "10:00",
-            "end": "11:00",
-            "busy": true
-        },
-        "3": {
-            "name": "CSSE332",
-            "start": "11:00",
-            "end": "12:00",
-            "busy": true
-        },
-        "4": {
-            "name": "Free",
-            "start": "12:00",
-            "end": "15:00",
-            "busy": false
-        },
-        "5": {
-            "name": "MA381",
-            "start": "15:00",
-            "end": "16:00",
-            "busy": true
-        }
+    4: {
+      name: "Free",
+      start: "12:00",
+      end: "13:00",
+      busy: false,
     },
-    "5": {
-        "1": {
-            "name": "ECONS152",
-            "start": "10:00",
-            "end": "11:00",
-            "busy": true
-        },
-        "2": {
-            "name": "CSSE332",
-            "start": "11:00",
-            "end": "12:00",
-            "busy": true
-        },
-        "3": {
-            "name": "Free",
-            "start": "12:00",
-            "end": "13:00",
-            "busy": false
-        },
-        "4": {
-            "name": "CSSE371",
-            "start": "13:00",
-            "end": "15:00",
-            "busy": true
-        },
-        "5": {
-            "name": "MA381",
-            "start": "15:00",
-            "end": "16:00",
-            "busy": true
-        }
-    }
+    5: {
+      name: "CSSE371",
+      start: "13:00",
+      end: "15:00",
+      busy: true,
+    },
+    6: {
+      name: "MA381",
+      start: "15:00",
+      end: "16:00",
+      busy: true,
+    },
+    7: {
+      name: "CSSE220 TA Meeting",
+      start: "16:00",
+      end: "17:00",
+      busy: true,
+    },
+  },
+  2: {
+    1: {
+      name: "CSSE220 TA",
+      start: "8:00",
+      end: "10:00",
+      busy: true,
+    },
+    2: {
+      name: "ECONS152",
+      start: "10:00",
+      end: "11:00",
+      busy: true,
+    },
+    3: {
+      name: "CSSE332",
+      start: "11:00",
+      end: "12:00",
+      busy: true,
+    },
+    4: {
+      name: "Free",
+      start: "12:00",
+      end: "15:00",
+      busy: false,
+    },
+    5: {
+      name: "MA381",
+      start: "15:00",
+      end: "16:00",
+      busy: true,
+    },
+    6: {
+      name: "Free",
+      start: "16:00",
+      end: "19:30",
+      busy: false,
+    },
+    7: {
+      name: "Concert Band",
+      start: "19:30",
+      end: "21:00",
+      busy: true,
+    },
+  },
+  3: {
+    1: {
+      name: "CSSE371",
+      start: "13:00",
+      end: "15:00",
+      busy: true,
+    },
+  },
+  4: {
+    1: {
+      name: "CSSE220 TA",
+      start: "8:00",
+      end: "10:00",
+      busy: true,
+    },
+    2: {
+      name: "ECONS152",
+      start: "10:00",
+      end: "11:00",
+      busy: true,
+    },
+    3: {
+      name: "CSSE332",
+      start: "11:00",
+      end: "12:00",
+      busy: true,
+    },
+    4: {
+      name: "Free",
+      start: "12:00",
+      end: "15:00",
+      busy: false,
+    },
+    5: {
+      name: "MA381",
+      start: "15:00",
+      end: "16:00",
+      busy: true,
+    },
+  },
+  5: {
+    1: {
+      name: "ECONS152",
+      start: "10:00",
+      end: "11:00",
+      busy: true,
+    },
+    2: {
+      name: "CSSE332",
+      start: "11:00",
+      end: "12:00",
+      busy: true,
+    },
+    3: {
+      name: "Free",
+      start: "12:00",
+      end: "13:00",
+      busy: false,
+    },
+    4: {
+      name: "CSSE371",
+      start: "13:00",
+      end: "15:00",
+      busy: true,
+    },
+    5: {
+      name: "MA381",
+      start: "15:00",
+      end: "16:00",
+      busy: true,
+    },
+  },
 };
--- script.js
+++ script.js
@@ -1,104 +1,142 @@
 let app = {};
 app.classes = classes;
-app.filters = document.querySelectorAll('.days-selector > .chip');
-app.events = document.querySelectorAll('.calendar > .card');
-const TIME_ZONE = 'America/Indianapolis';
+app.filters = document.querySelectorAll(".days-selector > .chip");
+app.events = document.querySelectorAll(".calendar > .card");
+const TIME_ZONE = "America/Indianapolis";
 
 /**
  * Adding an event listener to each of the filters.
-*/
+ */
 app.addListeners = () => {
-    app.filters.forEach((item) => {
-        item.onclick = () => {
-            app.updateView(item.dataset.day);
-            app.filters.forEach((f) => {
-                f.classList.remove('selected');
-                if (f.innerHTML.includes('symbols')) {
-                    f.innerHTML = f.innerHTML.split('<span class="material-symbols-rounded">done</span>')[1];
-                }
-            });
-            item.classList.add('selected');
-            item.innerHTML = '<span class="material-symbols-rounded">done</span>' + item.innerHTML;
+  app.filters.forEach((item) => {
+    item.onclick = () => {
+      app.updateView(item.dataset.day);
+      app.filters.forEach((f) => {
+        f.classList.remove("selected");
+        if (f.innerHTML.includes("symbols")) {
+          f.innerHTML = f.innerHTML.split(
+            '<span class="material-symbols-rounded">done</span>'
+          )[1];
         }
-    });
-}
+      });
+      item.classList.add("selected");
+      item.innerHTML =
+        '<span class="material-symbols-rounded">done</span>' + item.innerHTML;
+    };
+  });
+};
 
 /**
  * If skip, check the current time and select the corresponding filter.
  * Add a class to the event that is currently happening.
-*/
+ */
 app.useCurrentDate = (skip) => {
-    const d = new Date();
-    const local = d.toLocaleString();
-    const req = d.toLocaleString('en', { timeZone: TIME_ZONE });
-    app.offset = parseInt(local.split(' ')[1].split(':')[0]) - parseInt(req.split(' ')[1].split(':')[0]);
-    if (!skip && app.filters[d.getDay() - 1]) {
-        app.filters[d.getDay() - 1].click();
-        app.filters[d.getDay() - 1].scrollIntoView();
-        window.scrollTo(0, 0);
+  const d = new Date();
+  const local = d.toLocaleString();
+  const req = d.toLocaleString("en", { timeZone: TIME_ZONE });
+  app.offset =
+    parseInt(local.split(" ")[1].split(":")[0]) -
+    parseInt(req.split(" ")[1].split(":")[0]);
+  if (!skip && app.filters[d.getDay() - 1]) {
+    app.filters[d.getDay() - 1].click();
+    app.filters[d.getDay() - 1].scrollIntoView();
+    window.scrollTo(0, 0);
+  }
+  app.events.forEach((e) => e.classList.remove("current"));
+  for (let i = app.events.length - 1; i >= 0; i--) {
+    if (
+      app.events[i].dataset.start <= d.getHours() + d.getMinutes() / 60 &&
+      app.events[i].dataset.end > d.getHours() + d.getMinutes() / 60
+    ) {
+      app.events[i].classList.add("current");
+      if (!skip)
+        document.getElementById("rightNow").innerText =
+          app.events[i].querySelector(".card-title").innerText;
+      return;
     }
-    app.events.forEach((e) => e.classList.remove('current'));
-    for (let i = app.events.length - 1; i >= 0; i--) {
-        if (app.events[i].dataset.start <= (d.getHours() + d.getMinutes() / 60) && app.events[i].dataset.end > (d.getHours() + d.getMinutes() / 60)) {
-            app.events[i].classList.add('current');
-            if (!skip)
-                document.getElementById('rightNow').innerText = app.events[i].querySelector('.card-title').innerText;
-            return;
-        }
-    }
-}
+  }
+};
 
 /**
  * Creating the cards for the calendar.
-*/
+ */
 app.updateView = (day) => {
-    const data = document.querySelector('.calendar');
-    data.innerHTML = '';
-    if (!app.classes[day]) return;
-    for (let i = 1; i <= Object.keys(app.classes[day]).length; i++) {
-        const el = document.createElement('div');
-        el.classList.add('card');
-        if (app.classes[day][i]["busy"])
-            el.classList.add('busy');
-        let startNum = parseInt(app.classes[day][i]["start"].split(':')[0]) + parseInt(app.classes[day][i]["start"].split(':')[1]) / 60;
-        let endNum = parseInt(app.classes[day][i]["end"].split(':')[0]) + parseInt(app.classes[day][i]["end"].split(':')[1]) / 60;
-        startNum += app.offset;
-        endNum += app.offset;
-        el.setAttribute('data-start', startNum);
-        el.setAttribute('data-end', endNum);
-        el.setAttribute('style', '--size:' + (endNum - startNum));
-        const num = document.createElement('div');
-        num.classList.add('number');
-        num.innerHTML = '<span>' + i + '</span>';
-        el.appendChild(num);
-        const content = document.createElement('div');
-        content.classList.add('card-content');
-        if (Math.floor(startNum) == 0 || Math.floor(startNum) == 12)
-            startNum += 12;
-        if (Math.floor(endNum) == 0 || Math.floor(endNum) == 12)
-            endNum += 12;
-        const start = (startNum > 12) ? (Math.floor(startNum) - 12) + ':' + String((startNum - Math.floor(startNum)) * 60).padStart(2, '0').split('.')[0] + " PM" : Math.floor(startNum) + ':' + String((startNum - Math.floor(startNum)) * 60).padStart(2, '0').split('.')[0] + " AM";
-        const end = (endNum > 12) ? (Math.floor(endNum) - 12) + ':' + String((endNum - Math.floor(endNum)) * 60).padStart(2, '0').split('.')[0] + " PM" : Math.floor(endNum) + ':' + String((endNum - Math.floor(endNum)) * 60).padStart(2, '0').split('.')[0] + " AM";
-        content.innerHTML = `<h3 class="card-title">${app.classes[day][i]["name"]}</h3><h3 class="card-description">${start} - ${end}</h3>`;
-        el.appendChild(content);
-        data.appendChild(el);
-    }
-    app.events = document.querySelectorAll('.calendar > .card');
-    app.useCurrentDate(true);
-}
+  const data = document.querySelector(".calendar");
+  data.innerHTML = "";
+  if (!app.classes[day]) return;
+  for (let i = 1; i <= Object.keys(app.classes[day]).length; i++) {
+    const el = document.createElement("div");
+    el.classList.add("card");
+    if (app.classes[day][i]["busy"]) el.classList.add("busy");
+    let startNum =
+      parseInt(app.classes[day][i]["start"].split(":")[0]) +
+      parseInt(app.classes[day][i]["start"].split(":")[1]) / 60;
+    let endNum =
+      parseInt(app.classes[day][i]["end"].split(":")[0]) +
+      parseInt(app.classes[day][i]["end"].split(":")[1]) / 60;
+    startNum += app.offset;
+    endNum += app.offset;
+    el.setAttribute("data-start", startNum);
+    el.setAttribute("data-end", endNum);
+    el.setAttribute("style", "--size:" + (endNum - startNum));
+    const num = document.createElement("div");
+    num.classList.add("number");
+    num.innerHTML = "<span>" + i + "</span>";
+    el.appendChild(num);
+    const content = document.createElement("div");
+    content.classList.add("card-content");
+    if (Math.floor(startNum) == 0 || Math.floor(startNum) == 12) startNum += 12;
+    if (Math.floor(endNum) == 0 || Math.floor(endNum) == 12) endNum += 12;
+    const start =
+      startNum > 12
+        ? Math.floor(startNum) -
+          12 +
+          ":" +
+          String((startNum - Math.floor(startNum)) * 60)
+            .padStart(2, "0")
+            .split(".")[0] +
+          " PM"
+        : Math.floor(startNum) +
+          ":" +
+          String((startNum - Math.floor(startNum)) * 60)
+            .padStart(2, "0")
+            .split(".")[0] +
+          " AM";
+    const end =
+      endNum > 12
+        ? Math.floor(endNum) -
+          12 +
+          ":" +
+          String((endNum - Math.floor(endNum)) * 60)
+            .padStart(2, "0")
+            .split(".")[0] +
+          " PM"
+        : Math.floor(endNum) +
+          ":" +
+          String((endNum - Math.floor(endNum)) * 60)
+            .padStart(2, "0")
+            .split(".")[0] +
+          " AM";
+    content.innerHTML = `<h3 class="card-title">${app.classes[day][i]["name"]}</h3><h3 class="card-description">${start} - ${end}</h3>`;
+    el.appendChild(content);
+    data.appendChild(el);
+  }
+  app.events = document.querySelectorAll(".calendar > .card");
+  app.useCurrentDate(true);
+};
 
 /**
  * The main function of the app. It adds the event listeners to the filters, selects the current day, and adds the event listeners to the info icon and modal.
-*/
+ */
 app.main = () => {
-    app.addListeners();
-    app.useCurrentDate();
-    document.getElementById('infoIcon').onclick = () => {
-        $('#infoModal').slideDown(50);
-    }
-    document.getElementById('infoModal').onclick = () => {
-        $('#infoModal').slideUp(50);
-    }
-}
+  app.addListeners();
+  app.useCurrentDate();
+  document.getElementById("infoIcon").onclick = () => {
+    $("#infoModal").slideDown(50);
+  };
+  document.getElementById("infoModal").onclick = () => {
+    $("#infoModal").slideUp(50);
+  };
+};
 
 app.main();
