73a899a7c00341a909199841b4d0b7e3b4ad324d
Canon Maranda <22649817+cm090@users.noreply.github.com>
2023-08-20 20:25:32 -0400

    Update calendar for F23

--- data.js
+++ data.js
@@ -7,86 +7,44 @@
 const classes = {
   1: {
     1: {
-      name: "CSSE220 TA",
-      start: "8:00",
+      name: "CSSE304",
+      start: "9:00",
       end: "10:00",
       busy: true,
     },
     2: {
-      name: "ECONS152",
+      name: "CSSE374",
       start: "10:00",
-      end: "11:00",
-      busy: true,
-    },
-    3: {
-      name: "CSSE332",
-      start: "11:00",
       end: "12:00",
-      busy: true,
-    },
-    4: {
-      name: "Free",
-      start: "12:00",
-      end: "13:00",
-      busy: false,
-    },
-    5: {
-      name: "CSSE371",
-      start: "13:00",
-      end: "15:00",
-      busy: true,
-    },
-    6: {
-      name: "MA381",
-      start: "15:00",
-      end: "16:00",
-      busy: true,
-    },
-    7: {
-      name: "CSSE220 TA Meeting",
-      start: "16:00",
-      end: "17:00",
       busy: true,
     },
   },
   2: {
     1: {
-      name: "CSSE220 TA",
-      start: "8:00",
+      name: "CSSE304",
+      start: "9:00",
       end: "10:00",
       busy: true,
     },
     2: {
-      name: "ECONS152",
+      name: "Free",
       start: "10:00",
-      end: "11:00",
-      busy: true,
+      end: "13:00",
+      busy: false,
     },
     3: {
-      name: "CSSE332",
-      start: "11:00",
-      end: "12:00",
+      name: "CSSE372",
+      start: "13:00",
+      end: "14:00",
       busy: true,
     },
     4: {
       name: "Free",
-      start: "12:00",
-      end: "15:00",
-      busy: false,
-    },
-    5: {
-      name: "MA381",
-      start: "15:00",
-      end: "16:00",
-      busy: true,
-    },
-    6: {
-      name: "Free",
-      start: "16:00",
+      start: "14:00",
       end: "19:30",
       busy: false,
     },
-    7: {
+    5: {
       name: "Concert Band",
       start: "19:30",
       end: "21:00",
@@ -95,54 +53,22 @@ const classes = {
   },
   3: {
     1: {
-      name: "CSSE371",
-      start: "13:00",
-      end: "15:00",
+      name: "CSSE374",
+      start: "10:00",
+      end: "12:00",
       busy: true,
     },
   },
   4: {
     1: {
-      name: "CSSE220 TA",
-      start: "8:00",
+      name: "CSSE304",
+      start: "9:00",
       end: "10:00",
       busy: true,
     },
     2: {
-      name: "ECONS152",
+      name: "CSSE374",
       start: "10:00",
-      end: "11:00",
-      busy: true,
-    },
-    3: {
-      name: "CSSE332",
-      start: "11:00",
-      end: "12:00",
-      busy: true,
-    },
-    4: {
-      name: "Free",
-      start: "12:00",
-      end: "15:00",
-      busy: false,
-    },
-    5: {
-      name: "MA381",
-      start: "15:00",
-      end: "16:00",
-      busy: true,
-    },
-  },
-  5: {
-    1: {
-      name: "ECONS152",
-      start: "10:00",
-      end: "11:00",
-      busy: true,
-    },
-    2: {
-      name: "CSSE332",
-      start: "11:00",
       end: "12:00",
       busy: true,
     },
@@ -153,15 +79,17 @@ const classes = {
       busy: false,
     },
     4: {
-      name: "CSSE371",
+      name: "CSSE372",
       start: "13:00",
       end: "15:00",
       busy: true,
     },
-    5: {
-      name: "MA381",
-      start: "15:00",
-      end: "16:00",
+  },
+  5: {
+    1: {
+      name: "CSSE304",
+      start: "9:00",
+      end: "10:00",
       busy: true,
     },
   },
